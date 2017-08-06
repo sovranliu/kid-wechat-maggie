a3180153f579997ad5d93c2ac6089ffd8e049f98
keyanggui <273927457@qq.com>
2017-08-06 19:38:03 +0800

    getMessage

--- src/main/java/com/xyzq/kid/wechat/message/action/GetMessag.java
+++ src/main/java/com/xyzq/kid/wechat/message/action/GetMessag.java
@@ -1,9 +1,12 @@
 package com.xyzq.kid.wechat.message.action;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.google.gson.Gson;
 import com.xyzq.kid.wechat.action.member.WechatUserAjaxAction;
 import com.xyzq.kid.logic.message.dao.po.Message;
 import com.xyzq.kid.logic.message.service.MessageService;
@@ -13,20 +16,28 @@ import com.xyzq.simpson.maggie.access.spring.MaggieAction;
 import com.xyzq.simpson.maggie.framework.Context;
 import com.xyzq.simpson.maggie.framework.Visitor;
 
-@MaggieAction(path="kid/wechat/getMessageReply")
-public class GetMessagReply extends WechatUserAjaxAction {
+@MaggieAction(path="kid/wechat/getMessage")
+public class GetMessag extends WechatUserAjaxAction {
 	
 	@Autowired
 	UserService userService;
 	
 	@Autowired
 	MessageService messageService;
-
+	
+	Gson gson=new Gson();
+	
 	@Override
 	public String doExecute(Visitor visitor, Context context) throws Exception {
 		String mobileNo=(String)context.get(CONTEXT_KEY_MOBILENO);
 		UserEntity user=userService.selectByMolieNo(mobileNo);
-		List<Message> msgLit=messageService.queryAllMessageByUserId(user.id);
+		Message msg=messageService.queryLatestAnswer(user.id);
+		Map<String,String> map=new HashMap<>();
+		if(msg!=null){
+			map.put("content", msg.getAnswer());
+		}
+		context.set("0", "0");
+		context.set("data", gson.toJson(map));
 		return "success.json";
 	}
 	
