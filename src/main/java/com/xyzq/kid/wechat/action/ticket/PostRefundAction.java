558efdc989bf8c63c00f28c568ffdbe782186642
meiyejun <meuyejun@126.com>
2017-08-06 18:26:49 +0800

    meiyejun

--- src/main/java/com/xyzq/kid/wechat/action/ticket/PostRefundAction.java
+++ src/main/java/com/xyzq/kid/wechat/action/ticket/PostRefundAction.java
@@ -1,6 +1,8 @@
 package com.xyzq.kid.wechat.action.ticket;
 
+import com.xyzq.kid.CommonTool;
 import com.xyzq.kid.logic.ticket.entity.TicketEntity;
+import com.xyzq.kid.logic.ticket.entity.TicketRefundEntity;
 import com.xyzq.kid.logic.ticket.service.TicketService;
 import com.xyzq.kid.wechat.action.member.WechatUserAjaxAction;
 import com.xyzq.simpson.maggie.access.spring.MaggieAction;
@@ -8,11 +10,14 @@ import com.xyzq.simpson.maggie.framework.Context;
 import com.xyzq.simpson.maggie.framework.Visitor;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 增票
  */
-@MaggieAction(path = "kid/wechat/giveTicket")
-public class GiveTicketAction extends WechatUserAjaxAction {
+@MaggieAction(path = "kid/wechat/postRefund")
+public class PostRefundAction extends WechatUserAjaxAction {
     /**
      * Action中只支持Autowired注解引入SpringBean
      */
@@ -30,12 +35,15 @@ public class GiveTicketAction extends WechatUserAjaxAction {
     @Override
     public String doExecute(Visitor visitor, Context context) throws Exception {
 
-
-        String serialNumber = (String) context.parameter("serialNumber");
+        String serialNumber = (String) context.parameter("serialNumber", -1);
         TicketEntity ticketEntity = ticketService.getTicketsInfoBySerialno(serialNumber);
-        String mobileNo = (String)context.parameter("phone");
-
-        ticketService.handselTickets(ticketEntity.id, mobileNo);
+        TicketRefundEntity ticketRefundEntity = new TicketRefundEntity();
+        if(null != ticketEntity) {
+            ticketRefundEntity.deleted = CommonTool.STATUS_NORMAL;
+            ticketRefundEntity.status = TicketRefundEntity.REFUND_STATUS_NEW;
+            ticketRefundEntity.ticketid = ticketEntity.id;
+            ticketService.insertRefundSelective(ticketRefundEntity);
+        }
 
         return "success.json";
     }
--- src/main/java/com/xyzq/kid/wechat/action/ticket/ＧetRefundAction.java
+++ src/main/java/com/xyzq/kid/wechat/action/ticket/ＧetRefundAction.java
@@ -1,6 +1,8 @@
 package com.xyzq.kid.wechat.action.ticket;
 
+import com.google.gson.Gson;
 import com.xyzq.kid.logic.ticket.entity.TicketEntity;
+import com.xyzq.kid.logic.ticket.entity.TicketRefundEntity;
 import com.xyzq.kid.logic.ticket.service.TicketService;
 import com.xyzq.kid.wechat.action.member.WechatUserAjaxAction;
 import com.xyzq.simpson.maggie.access.spring.MaggieAction;
@@ -8,18 +10,23 @@ import com.xyzq.simpson.maggie.framework.Context;
 import com.xyzq.simpson.maggie.framework.Visitor;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 增票
  */
-@MaggieAction(path = "kid/wechat/giveTicket")
-public class GiveTicketAction extends WechatUserAjaxAction {
+@MaggieAction(path = "kid/wechat/getRefund")
+public class ＧetRefundAction extends WechatUserAjaxAction {
     /**
      * Action中只支持Autowired注解引入SpringBean
      */
     @Autowired
     private TicketService ticketService;
 
-
+    Gson gson=new Gson();
     /**
      * 动作执行
      *
@@ -30,13 +37,17 @@ public class GiveTicketAction extends WechatUserAjaxAction {
     @Override
     public String doExecute(Visitor visitor, Context context) throws Exception {
 
-
-        String serialNumber = (String) context.parameter("serialNumber");
+        String serialNumber = (String) context.parameter("serialNumber", -1);
         TicketEntity ticketEntity = ticketService.getTicketsInfoBySerialno(serialNumber);
-        String mobileNo = (String)context.parameter("phone");
+        TicketRefundEntity ticketRefundEntity =ticketService.loadRefundByTicketId(ticketEntity.id);
+        Map<String,Object> map=new HashMap<>();
+        if(null != ticketRefundEntity) {
+            map.put("expire", ticketEntity.expire);
+            map.put("price", ticketEntity.price);
+            map.put("serialNumber", ticketEntity.serialNumber);
+        }
 
-        ticketService.handselTickets(ticketEntity.id, mobileNo);
-
+        context.set("data", gson.toJson(map));
         return "success.json";
     }
 }
