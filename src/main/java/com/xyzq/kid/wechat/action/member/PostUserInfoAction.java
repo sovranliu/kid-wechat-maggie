568627730814297fdd9a65cb86361056f7c4563f
sovranliu <sovranliu@aliyun.com>
2017-08-14 23:47:07 +0800

    修改用户信息动作修改

--- src/main/java/com/xyzq/kid/wechat/action/member/PostUserInfoAction.java
+++ src/main/java/com/xyzq/kid/wechat/action/member/PostUserInfoAction.java
@@ -1,8 +1,8 @@
 package com.xyzq.kid.wechat.action.member;
 
-
 import java.util.Date;
 
+import com.xyzq.kid.common.service.SMSService;
 import com.xyzq.kid.logic.ticket.service.TicketService;
 import com.xyzq.kid.logic.user.entity.SessionEntity;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +14,16 @@ import com.xyzq.simpson.maggie.framework.Context;
 import com.xyzq.simpson.maggie.framework.Visitor;
 
 /**
- * 范例动作
+ * 修改用户信息
  */
 @MaggieAction(path = "kid/wechat/postUserInfo")
 public class PostUserInfoAction extends WechatUserAjaxAction {
-    /**
-     * Action中只支持Autowired注解引入SpringBean
-     */
     @Autowired
     private UserService userService;
     @Autowired
     private TicketService ticketService;
+    @Autowired
+    private SMSService smsService;
 
 
     /**
@@ -36,27 +35,42 @@ public class PostUserInfoAction extends WechatUserAjaxAction {
      */
     @Override
     public String doExecute(Visitor visitor, Context context) throws Exception {
-
-        UserEntity userEntity = new UserEntity();
-        userEntity.telephone = (String) context.get(WechatUserAjaxAction.CONTEXT_KEY_MOBILENO);
-        userEntity.userName = (String)context.parameter("userName");
-        userEntity.sex = (Integer)context.parameter("sex", 0);
-        userEntity.address = (String)context.parameter("address", "");
-        userEntity.subscribetime = CommonTool.dataToStringYMDHMS(new Date());
-        userService.updateByMobileNo(userEntity);
-
-        String telephoneNew = (String)context.parameter("telephone");
-        // 修改会话中的手机号码
-        String sId = visitor.cookie("sid");
-        SessionEntity sessionEntity = userService.fetchSession(sId);
-        sessionEntity.mobileNo = telephoneNew;
-        userService.saveSession(sessionEntity);
-
-        if(null != telephoneNew && telephoneNew.length() > 0 && !telephoneNew.equals(userEntity.telephone)) {
-            //更新用户手机号
-            userService.updateMobileNo(telephoneNew, userEntity.telephone);
-            //更新ticket表手机号
-//            ticketService.updateMobileNo(telephoneNew, userEntity.telephone);
+        String telephoneOld = (String) context.get(WechatUserAjaxAction.CONTEXT_KEY_MOBILENO);
+        String telephoneNew = (String) context.parameter("telephone");
+        if(("" + telephoneOld).equals(telephoneNew)) {
+            // 手机号码未修改
+            UserEntity userEntity = new UserEntity();
+            userEntity.telephone = telephoneOld;
+            userEntity.userName = (String) context.parameter("userName");
+            userEntity.sex = (Integer) context.parameter("sex", 0);
+            userEntity.address = (String) context.parameter("address", "");
+            userEntity.subscribetime = CommonTool.dataToStringYMDHMS(new Date());
+            userService.updateByMobileNo(userEntity);
+        }
+        else {
+            // 手机号码已修改
+            if(null != userService.selectByMolieNo(telephoneNew)) {
+                context.set("msg", "该手机号码已被注册");
+                return "fail.json";
+            }
+            if(!smsService.checkCaptcha(telephoneNew, (String) context.parameter("code"))) {
+                context.set("msg", "验证码不正确");
+                return "fail.json";
+            }
+            UserEntity userEntity = new UserEntity();
+            userEntity.telephone = telephoneOld;
+            userEntity.userName = (String) context.parameter("userName");
+            userEntity.sex = (Integer) context.parameter("sex", 0);
+            userEntity.address = (String) context.parameter("address", "");
+            userEntity.subscribetime = CommonTool.dataToStringYMDHMS(new Date());
+            userService.updateByMobileNo(userEntity);
+            // 更新用户手机号
+            userService.updateMobileNo(telephoneNew, telephoneOld);
+            // 修改会话中的手机号码
+            String sId = visitor.cookie("sid");
+            SessionEntity sessionEntity = userService.fetchSession(sId);
+            sessionEntity.mobileNo = telephoneNew;
+            userService.saveSession(sessionEntity);
         }
         return "success.json";
     }
